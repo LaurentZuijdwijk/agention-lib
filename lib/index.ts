c30e616b157a61fe60ffd257589c71e7f5158943
Laurent Zuijdwijk <laurent.zuijdwijk@gmail.com>
2025-04-19 19:12:28 +0100

    include more files in build

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 export * from "./agents/BaseAgent";
 export * from "./agents/anthropic/ClaudeAgent";
+export * from "./agents/openai/OpenAiAgent";
+export * from "./history/History";
+export * from "./graph/AgentGraph";
 export * from "./tools/Tool";
